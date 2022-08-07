f6ab65464e4592643ba2a8773504fae3a065cfc0
moresruth <moresruthantonette.com>
2022-08-07 15:24:11 +0800

    add assets

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import react from "react";
-import React from react;
+import react from 'react';
+import ReactDOM from 'react-dom'
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
