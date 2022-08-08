9f4828dcd8a2036b286bbe06ccb4242eea615b3b
moresruth <moresruthantonette.com>
2022-08-09 00:19:41 +0800

    add wrapper and connect sanity backend

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,33 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
+
+import { AppWrap } from '../../wrapper';
+// import { images } from '../../constants';
 import './About.scss';
+import { urlFor , client} from '../../client';
 
-const abouts = [
-  { title: 'Web Development',
-    description:'I am a good web developer',
-    imgUrl:images.about01
-  },
-
-  { title: 'Front-End Developer',
-    description:'I am a good web developer',
-    imgUrl:images.about02
-  },
-
-  { title: 'UX/UI Designer',
-    description:'I am a good web developer',
-    imgUrl:images.about03
-  },
-
-  { title: 'Web Animation',
-    description:'I am a good web developer',
-    imgUrl:images.about04
-  },
-
-]
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+  
+  
+
   return (
    <>
       <h2 className="head-text">I Know that <span>Good Design</span> <br/> means  <span>Good Business</span></h2>
@@ -40,7 +33,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index} >
 
-              <img src={about.imgUrl} alt={about.title} />
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
               <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
               <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
@@ -51,4 +44,4 @@ const About = () => {
   )
 }
 
-export default About;
+export default AppWrap(About, 'about');
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {motion } from 'framer-motion';
 
+
+import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
 
@@ -69,4 +71,4 @@ const Header = () => {
   )
 }
 
-export default Header;
+export default AppWrap(Header, 'home');
